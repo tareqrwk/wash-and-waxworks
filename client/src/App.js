ad831f2a2ca01c555fb791a5d2ed96fe25abd0eb
tareqrwk <tareqrwk@gmail.com>
2025-05-02 12:13:05 -0400

    Initial homepage with Hero section and Tailwind Setup

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import Hero from "./components/Hero";
+
+export default function App() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
---
+++ client/src/components/Hero.js
@@ -0,0 +1,11 @@
+function Hero(){
+    return(
+        /*Content */
+        <div className="h-screen bg-black text-white flex flex-col justify-center items-center text-center px-4">
+            <h1 className="text-5xl font-bold mb-4">Wash&WaxWorks Car Detailing</h1>
+            <p className="text-xl mb-6">Where Every Car Is Treated Like Royalty</p>
+            <button className="bg-blueViolet text-white px-6 py-3 rounded-full font-semibold hover:bg-wildBlue transition ">Book Now!</button>
+        </div>
+    )
+}
+export default Hero;
