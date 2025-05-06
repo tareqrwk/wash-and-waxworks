035f76f4d32aaf216bfd777937343c9d89e4ab6e
tareqrwk <tareqrwk@gmail.com>
2025-05-06 14:54:37 -0400

    Added security

--- server/index.js
+++ server/index.js
@@ -82,8 +82,8 @@ app.post('/api/contact', (req, res) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'washnwaxworks@gmail.com',
-            pass: 'fbou lerg odzn pclx'
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
         }
     });
 
